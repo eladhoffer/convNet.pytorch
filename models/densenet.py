acea1b3730cfe7ee2f04ae7d4885f4ba4370d204
Elad Hoffer <elad.hoffer@gmail.com>
2018-09-01 19:17:14 +0300

    update densenet

--- models/densenet.py
+++ models/densenet.py
@@ -8,7 +8,7 @@ __all__ = ['densenet']
 
 def init_model(model):
     # Official init from torch repo.
-    for m in self.modules():
+    for m in model.modules():
         if isinstance(m, nn.Conv2d):
             nn.init.kaiming_normal_(m.weight)
         elif isinstance(m, nn.BatchNorm2d):
@@ -72,19 +72,23 @@ class DenseNet(nn.Module):
         num_classes (int) - number of classification classes
     """
 
+    @staticmethod
+    def _create_features(num_features):
+        # First convolution
+        return nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_features,
+                                kernel_size=7, stride=2, padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+        ]))
+
     def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
                  num_init_features=64, bn_size=4, drop_rate=0, num_classes=1000):
 
         super(DenseNet, self).__init__()
 
-        # First convolution
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features,
-                                kernel_size=7, stride=2, padding=3, bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
-            ('relu0', nn.ReLU(inplace=True)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-        ]))
+        self.features = self._create_features(num_init_features)
 
         # Each denseblock
         num_features = num_init_features
@@ -104,6 +108,15 @@ class DenseNet(nn.Module):
 
         # Linear layer
         self.classifier = nn.Linear(num_features, num_classes)
+        init_model(self)
+
+    @staticmethod
+    def regularization_pre_step(model, weight_decay=1e-4):
+        with torch.no_grad():
+            for m in model.modules():
+                if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                    m.weight.grad.add_(weight_decay * m.weight)
+        return 0
 
     def forward(self, x):
         features = self.features(x)
@@ -114,49 +127,63 @@ class DenseNet(nn.Module):
         return out
 
 
-class DenseNet_cifar(nn.Module):
-    r"""Densenet-BC model class, based on
-    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
-
-    Args:
-        growth_rate (int) - how many filters to add each layer (`k` in paper)
-        block_config (list of 4 ints) - how many layers in each pooling block
-        num_init_features (int) - the number of filters to learn in the first convolution layer
-        bn_size (int) - multiplicative factor for number of bottle neck layers
-          (i.e. bn_size * k features in the bottleneck layer)
-        drop_rate (float) - dropout rate after each dense layer
-        num_classes (int) - number of classification classes
-    """
-
+class DenseNet_imagenet(DenseNet):
     def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
-                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=10):
+                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=1000, regime='normal', scale_lr=1):
+        super(DenseNet_imagenet, self).__init__(growth_rate, block_config,
+                                             num_init_features, bn_size, drop_rate, num_classes)
 
-        super(DenseNet_cifar, self).__init__()
+        def ramp_up_lr(lr0, lrT, T):
+            rate = (lrT - lr0) / T
+            return "lambda t: {'lr': %s + t * %s}" % (lr0, rate)
+        if regime == 'normal':
+            self.regime = [
+                {'epoch': 0, 'optimizer': 'SGD', 'momentum': 0.9,
+                    'step_lambda': ramp_up_lr(0.1, 0.1 * scale_lr, 5004 * 5 / scale_lr)},
+                {'epoch': 5,  'lr': scale_lr * 1e-1},
+                {'epoch': 30, 'lr': scale_lr * 1e-2},
+                {'epoch': 60, 'lr': scale_lr * 1e-3},
+                {'epoch': 80, 'lr': scale_lr * 1e-4}
+            ]
+        elif regime == 'small':
+            scale_lr *= 4
+            self.regime = [
+                {'epoch': 0, 'optimizer': 'SGD',
+                    'momentum': 0.9, 'lr': scale_lr * 1e-1},
+                {'epoch': 30, 'lr': scale_lr * 1e-2},
+                {'epoch': 60, 'lr': scale_lr * 1e-3},
+                {'epoch': 80, 'lr': scale_lr * 1e-4}
+            ]
+            self.data_regime = [
+                {'epoch': 0, 'input_size': 128, 'batch_size': 256},
+                {'epoch': 80, 'input_size': 224, 'batch_size': 64},
+            ]
+            self.data_eval_regime = [
+                {'epoch': 0, 'input_size': 128, 'batch_size': 1024},
+                {'epoch': 80, 'input_size': 224, 'batch_size': 512},
+            ]
 
+
+class DenseNet_cifar(DenseNet):
+
+    @staticmethod
+    def _create_features(num_features):
         # First convolution
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features,
+        return nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_features,
                                 kernel_size=3, stride=1, padding=1, bias=False))
         ]))
 
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
-                                bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate)
-            self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(
-                    num_input_features=num_features, num_output_features=num_features // 2)
-                self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = num_features // 2
+    def __init__(self, *kargs, **kwargs):
 
-        # Final batch norm
-        self.features.add_module('norm4', nn.BatchNorm2d(num_features))
+        super(DenseNet_cifar, self).__init__(*kargs, **kwargs)
 
-        # Linear layer
-        self.classifier = nn.Linear(num_features, num_classes)
+        self.regime = [
+            {'epoch': 0, 'optimizer': 'SGD', 'lr': 1e-1,
+                'weight_decay': 0, 'momentum': 0.9},
+            {'epoch': 150, 'lr': 1e-2},
+            {'epoch': 225, 'lr': 1e-3},
+        ]
 
     def forward(self, x):
         features = self.features(x)
@@ -191,7 +218,7 @@ def densenet(**config):
             config.update(dict(num_init_features=96,
                                growth_rate=48, block_config=(6, 12, 36, 24)))
 
-        return DenseNet(**config)
+        return DenseNet_imagenet(**config)
 
     elif dataset == 'cifar10':
         config.setdefault('num_classes', 10)
