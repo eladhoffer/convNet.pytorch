cdae889a64f0fcd965ce063e0017a639e763b814
Elad Hoffer <elad.hofer@gmail.com>
2019-06-04 17:49:27 +0300

    enable jit

--- models/modules/activations.py
+++ models/modules/activations.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-# @torch.jit.script
+@torch.jit.script
 def swish(x):
     # type: (Tensor) -> Tensor
     return x * x.sigmoid()
 
 
-# @torch.jit.script
+@torch.jit.script
 def hard_sigmoid(x):
     # type: (Tensor) -> Tensor
     return F.relu6(x+3).div_(6)
 
 
-# @torch.jit.script
+@torch.jit.script
 def hard_swish(x):
     # type: (Tensor) -> Tensor
     return x * hard_sigmoid(x)
